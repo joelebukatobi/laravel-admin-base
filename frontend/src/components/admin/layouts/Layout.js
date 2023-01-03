44dd7eb104e9531564d6874266fe20db5abe08da
Joel Onwuanaku <joelebuka@gmail.com>
2023-01-03 07:55:01 +0100

    Initial Commit

---
+++ frontend/src/components/admin/layouts/Layout.js
@@ -0,0 +1,38 @@
+// React
+import { useEffect } from 'react';
+// Next JS
+import { useRouter } from 'next/router';
+// Components
+import Navbar from '@/admin//layouts/Navbar';
+import Body from '@/admin//layouts/Body';
+import Sidebar from '@/admin//layouts//Sidebar';
+import Loading from '@/admin//components//Loading';
+import Login from '@/admin//components//Login';
+// Config & Helpers
+import { API_URL } from '@/config/index';
+// External Libraries
+import { useDispatch, useSelector } from 'react-redux';
+import { getUser } from '@/features//user/userActions';
+
+export default function Layout({ children }) {
+  const { data } = useSelector((state) => state.user);
+  const navigate = useRouter().push;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+
+  return (
+    <div id="admin">
+      {data ? (
+        <>
+          <Navbar />
+          <Sidebar />
+          <Body>{children}</Body>
+        </>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
+}
